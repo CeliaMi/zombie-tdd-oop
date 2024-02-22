0af2fd095749d05dcca374330371f2a575c350e2
Celia <celiamillanalemany@gmail.com>
2024-02-22 13:14:53 +0100

    refactor test with beforeEach

--- tests/Survivor.test.ts
+++ tests/Survivor.test.ts
@@ -1,34 +1,39 @@
 import { Survivor } from '../src/Survivor';
 
 describe('Survivor', () => { 
-    
+
+    let survivor : Survivor = {} as Survivor
+    beforeEach(()=>{
+        survivor = new Survivor('juana')
+    })
+
     test('Every survivor should have a Name', ()=> {
-        const survivor = new Survivor('juana')
+
         const sut = survivor.name
         expect(sut).toBe('juana');
     })
 
     test('Each survivor should start with 0 wounds', ()=> {
-        const survivor = new Survivor('juana')
+
         const sut = survivor.getWounds()
          expect(sut).toBe(0);
     })
 
     test('A survivor who receives 2 Wounds should die', ()=> {
-        const survivor = new Survivor('juana')
+
         survivor.injure(2);
         const sut = survivor.getAlive()
         expect(sut).toBe(false);
     })
 
     test('Each survivor should start with the ability to perform 3 actions per shift', ()=> {
-        const survivor = new Survivor('juana')
+   
         const sut = survivor.getActions().length
         expect(sut).toBe(3);
     })
 
     test('Each Survivor can carry up to 5 pieces of Equipment', ()=> {
-        const survivor = new Survivor('juana')
+  
         const EquipamentsToAdd = ['Baseball Bat', 'Flashlight', 'Gasoline', 'Axe', 'Pistol']
         EquipamentsToAdd.forEach(equipament => survivor.addEquipament(equipament))
         const sut = survivor.getEquipment().length
@@ -36,7 +41,7 @@ describe('Survivor', () => {
     })
 
     test('Each Survivor can´t carry up more than 5 pieces of Equipment', ()=> {
-        const survivor = new Survivor('juana')
+
         const EquipamentsToAdd = ['Baseball Bat', 'Flashlight', 'Gasoline', 'Axe', 'Pistol', 'Knife']
         EquipamentsToAdd.forEach(equipament => survivor.addEquipament(equipament))
         const sut = survivor.getEquipment().length
@@ -44,7 +49,7 @@ describe('Survivor', () => {
     })
 
     test('When a survivor tries to have more than 5 pieces of equipment the program warns that it is not possible', ()=> {
-        const survivor = new Survivor('juana')
+
         const EquipamentsToAdd = ['Baseball Bat', 'Flashlight', 'Gasoline', 'Axe', 'Pistol']
         EquipamentsToAdd.forEach(equipament => survivor.addEquipament(equipament))
         const sut = survivor.addEquipament('Knife')
