5e231e4189839afa2c38de17751c2a21e8214d03
Celia <celiamillanalemany@gmail.com>
2024-02-22 09:44:54 +0100

    Implement Survivor injure method functionality (Test Success)

--- src/Survivor.ts
+++ src/Survivor.ts
@@ -1,10 +1,17 @@
 export class Survivor {
     public name: string;
     public wounds: number 
+    public alive: boolean;
     
     constructor(name : string) {
         this.name = name;
         this.wounds = 0;
+        this.alive = true;
     }
-  
+    injure(attack = 1) {
+        this.wounds += attack;
+        if (this.wounds >= 2) {
+           this.alive = false;
+        }
+    }
 }
