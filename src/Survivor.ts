a94ef25681b9fe42c1ede8695d55be4288cacef1
Celia <celiamillanalemany@gmail.com>
2024-02-22 09:35:09 +0100

    Implement Survivor wounds functionality (Test Success)

--- src/Survivor.ts
+++ src/Survivor.ts
@@ -1,8 +1,10 @@
 export class Survivor {
-    public name: string; 
+    public name: string;
+    public wounds: number 
     
     constructor(name : string) {
         this.name = name;
+        this.wounds = 0;
     }
   
 }
