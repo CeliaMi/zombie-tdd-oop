526bf498d0ce3b5a175dee6f7cda2c6fa050394d
Celia <celiamillanalemany@gmail.com>
2024-02-22 09:31:29 +0100

    Add unit test for Survivor wounds (Test Failure)

--- src/Survivor.ts
+++ src/Survivor.ts
@@ -1,5 +1,5 @@
 export class Survivor {
-    public name: string;
+    public name: string; 
     
     constructor(name : string) {
         this.name = name;
--- tests/Survivor.test.ts
+++ tests/Survivor.test.ts
@@ -1,11 +1,17 @@
 import { Survivor } from '../src/Survivor';
 
-describe('Survival', () => { 
+describe('Survivor', () => { 
     
     test('Every survivor should have a Name', ()=> {
         const survivor = new Survivor('juana')
         const sut = survivor.name
         expect(sut).toBe('juana');
+    })
+
+    test('Each survivor should start with 0 wounds', ()=> {
+        const survivor = new Survivor('juana')
+        const sut = survivor.wounds
+         expect(sut).toBe(0);
     })
 
 })
