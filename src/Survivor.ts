bf61043c9b39616c74f6efa798e9cada12551082
Celia <celiamillanalemany@gmail.com>
2024-02-22 10:23:15 +0100

    Fix and pass unit test for Survivor can carry up more than 5 pieces of equipment (Test Success)

--- src/Survivor.ts
+++ src/Survivor.ts
@@ -6,6 +6,7 @@ export class Survivor {
     public alive: boolean;
     public actions: string[];
     public equipment: string[];
+    public permittedEquipment: number;
     
     constructor(name : string) {
         this.name = name;
@@ -13,7 +14,9 @@ export class Survivor {
         this.alive = true;
         this.actions = defaultActions;
         this.equipment = [];
+        this.permittedEquipment = 5;
     }
+    
     public injure(attack = 1) {
         this.wounds += attack;
         if (this.wounds >= 2) {
@@ -22,7 +25,11 @@ export class Survivor {
     }
 
     addEquipament(newEquipment : string ){
+        if(this.equipment.length < this.permittedEquipment){
             this.equipment.push(newEquipment)
+        } else {
+            console.log(`The maximum number of pieces of equipment is ${this.permittedEquipment}`);
         }
+    }
     
 }
